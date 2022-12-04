43faf06f4c570850084b75aca668c03d3cadf16e
Tomek28611 <tomek286@yahoo.com>
2022-12-04 11:27:38 +0100

    commit

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -18,15 +18,15 @@ const Navbar = () => (
       <img
         src="/search.svg"
         alt="search"
-        className="w-[24px] h-[24px] object-contain"
+        className="w-[28px] h-[28px] object-contain"
       />
       <h2 className="font-extrabold text-[24px] leading-[30.24px] text-white">
-        ABC TRAVEL
+        ABC TRAVEL Company
       </h2>
       <img
         src="/menu.svg"
         alt="menu"
-        className="w-[24px] h-[24px] object-contain"
+        className="w-[28px] h-[28px] object-contain"
       />
     </div>
   </motion.nav>
