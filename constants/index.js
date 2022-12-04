62f263da9c3d1bbfa1362d2f4a7df0472604e117
Tomek28611 <tomek286@yahoo.com>
2022-12-04 07:16:11 +0100

    commit

---
+++ constants/index.js
@@ -0,0 +1,88 @@
+export const exploreWorlds = [
+  {
+    id: 'world-1',
+    imgUrl: '/world-01.png',
+    title: 'Explore Cuba',
+  },
+  {
+    id: 'world-2',
+    imgUrl: '/world-02.webp',
+    title: 'Explore Thailand',
+  },
+  {
+    id: 'world-3',
+    imgUrl: '/world-03.jpg',
+    title: 'Explore Australia',
+  },
+  {
+    id: 'world-4',
+    imgUrl: '/world-04.jfif',
+    title: 'Explore Paris',
+  },
+  {
+    id: 'world-5',
+    imgUrl: '/world-05.jpg',
+    title: 'Maybe Grand Canyon',
+  },
+];
+
+export const startingFeatures = [
+  'Find a place where you want to go',
+  'Contact with us',
+  'and have fun...',
+];
+
+export const newFeatures = [
+  {
+    imgUrl: '/vrpano.svg',
+    title: 'Campus in the Nature',
+    subtitle:
+        'campus in the mountains, wooden houses, electronics forbidden. Good contact with nature and animals',
+  },
+  {
+    imgUrl: '/headset.svg',
+    title: 'Campus in city ',
+    subtitle:
+        'campus in the big city, restaurants, cinemas, video games and many other attractions',
+  },
+];
+
+export const insights = [
+  {
+    imgUrl: '/ins-01.webp',
+    title: 'great trip, you can say a trip of a lifetime',
+    subtitle:
+        'Travel is the movement of people between distant geographical locations. Travel can be done by foot, bicycle, automobile, train, boat, bus, airplane, ship or other means, with or without luggage, and can be one way or round trip..',
+  },
+  {
+    imgUrl: '/ins-02.webp',
+    title: 'great trip, you can say a trip of a lifetime',
+    subtitle:
+        'Travel is the movement of people between distant geographical locations. Travel can be done by foot, bicycle, automobile, train, boat, bus, airplane, ship or other means, with or without luggage, and can be one way or round trip.',
+  },
+  {
+    imgUrl: '/ins-03.jfif',
+    title: 'great trip, you can say a trip of a lifetime',
+    subtitle:
+        'Travel is the movement of people between distant geographical locations. Travel can be done by foot, bicycle, automobile, train, boat, bus, airplane, ship or other means, with or without luggage, and can be one way or round trip.',
+  },
+];
+
+export const socials = [
+  {
+    name: 'twitter',
+    url: '/twitter.svg',
+  },
+  {
+    name: 'linkedin',
+    url: '/linkedin.svg',
+  },
+  {
+    name: 'instagram',
+    url: '/instagram.svg',
+  },
+  {
+    name: 'facebook',
+    url: '/facebook.svg',
+  },
+];
